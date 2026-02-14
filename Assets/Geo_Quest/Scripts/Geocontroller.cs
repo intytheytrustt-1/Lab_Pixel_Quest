f93b196d85fef424b9e30630648225e4767ad746
intytheytrustt-1 <intytheytrust@gmail.com>
2026-02-14 16:32:54 -0500

    hello wolrd code change
    hello world coding changes

---
+++ Assets/Geo_Quest/Scripts/Geocontroller.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    string varOne = "Hello World";
+    void Start()
+    {
+        string varTwo = "World";
+        Debug.Log(varOne + varTwo); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

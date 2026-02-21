2062e624b8a9821d8c051502bf0ee6bb0f8054db
intytheytrustt-1 <intytheytrust@gmail.com>
2026-02-21 16:23:39 -0500

    moovement changes in code( vectors)

--- Assets/Geo_Quest/Scripts/Geocontroller.cs
+++ Assets/Geo_Quest/Scripts/Geocontroller.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Geocontroller : MonoBehaviour
 {
     // Start is called before the first frame update
     string varOne = "Hello World";
+    int counter = 0;
     void Start()
     {
         string varTwo = "World";
@@ -13,8 +14,16 @@ public class NewBehaviourScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+      
+
+         counter = counter + 1;
+        Debug.Log(counter);
     }
+    
 }
